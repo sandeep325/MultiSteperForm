10817a71937852c208d43eae64a3b180a3b785db
sandeep325 <negisandeep9819@gmail.com>
2024-09-18 16:58:57 +0530

    commit

---
+++ src/Components/Common/Style.jsx
@@ -0,0 +1,9 @@
+export const Styles = {
+    formcontainer:{
+           width:"100%",
+           height:"98vh",
+           display:"flex",
+           flexFlow:"row wrap",
+           justifyContent:"center"
+    }
+}
---
+++ src/Components/FormComponents.jsx
@@ -0,0 +1,14 @@
+import { Grid2 } from '@mui/material';
+import React, { Component } from 'react'
+
+export default class FormComponents extends Component {
+    render() {
+        return (
+            <>
+                <Grid2 container>
+                    <Grid2 item xs={12} sm={7}></Grid2>
+                </Grid2>
+            </>
+        )
+    }
+}
